54e78975c1c6c62574b7f5c46101726fae6e1e65
adamaryromero <adamaryromerog@gmail.com>
2026-07-02 23:41:40 -0500

    Add files via upload

--- src/routes/pedidos.routes.js
+++ src/routes/pedidos.routes.js
@@ -1,19 +1,14 @@
 import { Router } from "express";
-import { 
-    getPedidos, 
-    getPedidosxid, 
-    postPedidos, 
-    putPedidos, 
-    deletePedidos 
-} from "../controladores/pedidosctrl.js";
+import { verificarToken } from '../middlewares/auth.js';
+import { getPedidos, getPedidosxid, postPedidos, putPedidos, deletePedidos } from "../controladores/pedidosctrl.js";
 
 const router = Router();
 
-router.get("/pedidos", getPedidos);
-router.get("/pedidos/:id", getPedidosxid);
-router.post("/pedidos", postPedidos);
-router.put("/pedidos/:id", putPedidos);
-router.patch("/pedidos/:id", putPedidos);
-router.delete("/pedidos/:id", deletePedidos);
+router.get("/pedidos", verificarToken, getPedidos);
+router.get("/pedidos/:id", verificarToken, getPedidosxid);
+router.post("/pedidos", verificarToken, postPedidos);
+router.put("/pedidos/:id", verificarToken, putPedidos);
+router.patch("/pedidos/:id", verificarToken, putPedidos);
+router.delete("/pedidos/:id", verificarToken, deletePedidos);
 
 export default router;
