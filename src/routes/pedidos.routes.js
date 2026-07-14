b6e373d6dbd07998262d69f7a78bafa3fb432519
adamaryromero <adamaryromerog@gmail.com>
2026-07-14 00:54:58 -0500

    Add files via upload

--- src/routes/pedidos.routes.js
+++ src/routes/pedidos.routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { verificarToken } from '../middlewares/auth.js';
-import { getPedidos, getPedidosxid, postPedidos, putPedidos, deletePedidos } from "../controladores/pedidosctrl.js";
+import { getPedidos, getPedidosxid, postInsertarPedido, putPedidos, deletePedidos } from "../controladores/pedidosctrl.js";
 import { generarPDFPedido } from "../controladores/pedidospdfctrl.js";
 
 const router = Router();
 
 router.get("/pedidos", verificarToken, getPedidos);
 router.get("/pedidos/:id", verificarToken, getPedidosxid);
-router.post("/pedidos", verificarToken, postPedidos);
+router.post("/pedidos", verificarToken, postInsertarPedido);
 router.put("/pedidos/:id", verificarToken, putPedidos);
 router.patch("/pedidos/:id", verificarToken, putPedidos);
 router.delete("/pedidos/:id", verificarToken, deletePedidos);
